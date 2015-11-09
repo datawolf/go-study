9d29381075ac4deeda9ed78ea7c5afe97e5dc246
Wang Long <wanglong@laoqinren.net>
2015-11-09 19:40:38 +0800

    Add URL Parsing example
    Signed-off-by: Wang Long <wanglong@laoqinren.net>

---
+++ url-parsing/main.go
@@ -0,0 +1,43 @@
+//
+// main.go
+// Copyright (C) 2015 wanglong <wanglong@SZX1000042009>
+//
+// Distributed under terms of the MIT license.
+//
+
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/url"
+)
+
+func main() {
+	s := "qvod://user:password@localhost.com:1234/path?k=v#f"
+
+	fmt.Println("URL:", s)
+	u, err := url.Parse(s)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(u.Scheme)
+	fmt.Println(u.User)
+	fmt.Println(u.User.Username())
+	p, _ := u.User.Password()
+	fmt.Println(p)
+
+	fmt.Println(u.Host)
+	host, port, _ := net.SplitHostPort(u.Host)
+	fmt.Println(host)
+	fmt.Println(port)
+
+	fmt.Println(u.Path)
+	fmt.Println(u.Fragment)
+
+	fmt.Println(u.RawQuery)
+	m, _ := url.ParseQuery(u.RawQuery)
+	fmt.Println(m)
+	fmt.Println(m["k"][0])
+}
