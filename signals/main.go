bcdce26b052b4d58266665d1c537e7d31381108f
Wang Long <wanglong@laoqinren.net>
2015-11-10 11:24:47 +0800

    Add signals example
    Signed-off-by: Wang Long <wanglong@laoqinren.net>

---
+++ signals/main.go
@@ -0,0 +1,33 @@
+//
+// main.go
+// Copyright (C) 2015 wanglong <wanglong@SZX1000042009>
+//
+// Distributed under terms of the MIT license.
+//
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("exiting")
+}
