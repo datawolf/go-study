89acfeb63c12314a1f0f03c5ab65c50eb239f4c6
Wang Long <wanglong@laoqinren.net>
2015-12-02 20:58:02 +0800

    add markdown-web-app example
    Signed-off-by: Wang Long <wanglong@laoqinren.net>

---
+++ building-web-apps-with-go/markdown-web-app/main.go
@@ -0,0 +1,24 @@
+//
+// main.go
+// Copyright (C) 2015 wanglong <wanglong@SZX1000042009>
+//
+// Distributed under terms of the MIT license.
+//
+
+package main
+
+import (
+	"net/http"
+	"github.com/russross/blackfriday"
+)
+
+func main() {
+	http.HandleFunc("/markdown", GenerateMarkdown)
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":8080", nil)
+}
+
+func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+	rw.Write(markdown)
+}
