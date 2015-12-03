dc9795d901a82c8c203da3af019b65c15a4ca7db
Wang Long <wanglong@laoqinren.net>
2015-12-03 14:36:52 +0800

    Add middleware example
    Signed-off-by: Wang Long <wanglong@laoqinren.net>

---
+++ building-web-apps-with-go/middleware-stack/main.go
@@ -0,0 +1,39 @@
+//
+// main.go
+// Copyright (C) 2015 wanglong <wanglong@SZX1000042009>
+//
+// Distributed under terms of the MIT license.
+//
+
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/codegangsta/negroni"
+)
+
+func main() {
+	// Middleware stack
+	n := negroni.New(
+		negroni.NewRecovery(),
+		negroni.HandlerFunc(MyMiddleware),
+		negroni.NewLogger(),
+		negroni.NewStatic(http.Dir(".")),
+	)
+
+	n.Run(":8080")
+}
+
+func MyMiddleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	log.Println("Logging on the way there...")
+
+	if r.URL.Query().Get("password") == "secret123" {
+		next(rw, r)
+	} else {
+		http.Error(rw, "Not Authorized", 401)
+	}
+
+	log.Println("Logging on the way back...")
+}
