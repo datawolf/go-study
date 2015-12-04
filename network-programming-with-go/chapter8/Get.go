1e9f7bd1fd2b5064633218b8d50a332be492eeb8
Wang Long <wanglong@laoqinren.net>
2015-12-04 12:24:55 +0800

    Add GET example
    Signed-off-by: Wang Long <wanglong@laoqinren.net>

---
+++ network-programming-with-go/chapter8/Get.go
@@ -0,0 +1,48 @@
+//
+// Get.go
+// Copyright (C) 2015 wanglong <wanglong@SZX1000042009>
+//
+// Distributed under terms of the MIT license.
+//
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ", os.Args[0], "host:port")
+		os.Exit(1)
+	}
+
+	url := os.Args[1]
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(2)
+	}
+
+	if response.Status != "200 OK" {
+		fmt.Println(response.Status)
+		os.Exit(2)
+	}
+
+	b, _ := httputil.DumpResponse(response, false)
+	fmt.Print(string(b))
+
+	var buf [512]byte
+	reader := response.Body
+	for {
+		n, err := reader.Read(buf[0:])
+		if err != nil {
+			os.Exit(0)
+		}
+		fmt.Print(string(buf[0:n]))
+	}
+	os.Exit(0)
+}
