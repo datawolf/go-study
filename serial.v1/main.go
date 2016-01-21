4c5f8018426a066018d2747bc8baf4d37df3e419
Wang Long <wanglong@laoqinren.net>
2016-01-21 21:41:44 +0800

    add chanel to main.go
    Signed-off-by: Wang Long <wanglong@laoqinren.net>

--- serial.v1/main.go
+++ serial.v1/main.go
@@ -8,14 +8,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/lnmx/serial"
 	"gopkg.in/ini.v1"
+)
+
+type Id struct {
+	ic string `json:"id"`
+}
+
+var (
+	num = make(chan string, 1)
 )
 
 func main() {
@@ -33,13 +43,37 @@ func main() {
 	setupSerial(device, baud)
 }
 
-func serialServer(){
-	http.Handle("/id", getID)
+func serialServer() {
+	http.HandleFunc("/id", getID)
 	http.ListenAndServe(":8080", nil)
 }
 
 func getID(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "id=1232332")
+	temp_num := ""
+	timeout := make(chan bool, 1)
+	go func() {
+		time.Sleep(1*time.Second)
+		timeout <- true
+	}()
+
+	select {
+	case temp_num = <-num:
+		log.Println("get id ok")
+	case <-timeout:
+		temp_num = "404"
+		log.Println("get id timeout")
+	}
+
+	id := Id{
+		ic: temp_num,
+	}
+
+	log.Println("id:", id)
+	log.Println("temp_num:", temp_num)
+	res.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	if err := json.NewEncoder(res).Encode(id); err != nil {
+		log.Println("Encode error")
+	}
 }
 
 func setupSerial(device string, baud int) {
@@ -53,11 +87,23 @@ func setupSerial(device string, baud int) {
 	log.Println("ready")
 
 	buf := make([]byte, 512)
-
+	timeout := make(chan bool, 1)
 	for {
 		n, err := port.Read(buf)
 		if err != nil {
 			log.Println("serial read error:", err)
+		}
+
+		go func() {
+			time.Sleep(1*time.Second)
+			timeout <- true
+		}()
+
+		select {
+		case num <- string(buf[:n]):
+			log.Println("setupserail ok")
+		case <-timeout:
+			log.Println("setupserail timeout")
 		}
 		// n = the read length
 		if n > 0 {
