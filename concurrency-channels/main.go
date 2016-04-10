8e06384975f41cb4db8f88b888eda148f6e9196f
Wang Long <long.wanglong@huawei.com>
2016-04-10 21:40:45 +0800

    add concurrency-channels example
    Signed-off-by: Wang Long <long.wanglong@huawei.com>

---
+++ concurrency-channels/main.go
@@ -0,0 +1,36 @@
+//
+// main.go
+// Copyright (C) 2016 datawolf <datawolf@datawolf-Lenovo-G460>
+//
+// Distributed under terms of the MIT license.
+//
+
+package main
+
+import "fmt"
+
+func main() {
+	n := 10
+	c := make(chan int)
+	done := make(chan bool)
+
+	go func() {
+		for i := 0; i < 1000; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+
+	for i := 0; i < n; i++ {
+		go func(x int) {
+			for q := range c {
+				fmt.Println("i", x, "q", q)
+			}
+			done <- true
+		}(i)
+	}
+
+	for i := 0; i < n; i++ {
+		<-done
+	}
+}
